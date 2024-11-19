9d5e296190795ff42a40dc175f09cdc780dca6a0
handic <handic@protel.com.tr>
2024-11-19 13:13:35 +0300

    Initial Commit

---
+++ CardFlipper/CardFlipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardFlipperApp.swift
+//  CardFlipper
+//
+//  Created by handic on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CardFlipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
